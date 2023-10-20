80b0ad969d117ace079b7a4a9187a2c74e8099ea
Tesseract <146233547+SholichID3@users.noreply.github.com>
2023-10-20 09:17:10 +0700

    Update subscriber.py

--- combine/src/subscriber.py
+++ combine/src/subscriber.py
@@ -3,7 +3,7 @@ import rospy
 from std_msgs.msg import String
 
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    rospy.loginfo(rospy.get_caller_id() + " I heard %s", data.data)
     
 def subscriber_str():
     rospy.init_node('subscriber', anonymous=True)
